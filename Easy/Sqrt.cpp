92b70e02e51df07bc53a48af97fd8279def1ed21
BluBloos <cnoah1705@gmail.com>
2022-07-22 08:42:17 -0400

    integer sqrt is now very fast as a binary search algo

--- Easy/Sqrt.cpp
+++ Easy/Sqrt.cpp
@@ -1,28 +1,27 @@
 class Solution {
+    //std::vector<int> pSquares = {};
+    //bool init = false;        
 public:
     int mySqrt(int x) {
-        // the most stupid thing to do is like, for loop
-        // up thru n until you hit the value that is good
-        int n = 0;
-        while (true) {
-            uint64_t sq = (uint64_t )n * n;
-            if (sq  > x) {
-                return n - 1;
-            } else if (sq == x) {
-                return n;
+        // we want to probably do a binary search.
+        // and like, that's it, right?
+        int mi = (46340 + 1) / 2;
+        int a = 0; // lower bound
+        int b = (46340 + 1) - 1; // upper bound
+        for (; true; ) {
+            if ( mi * mi == x ) {
+                return mi;
+            } else if ( mi * mi > x ) {
+                // left search
+                b = mi - 1;
+            } else {
+                a = mi + 1;                
             }
-            n++;
+            if (a > b) {
+                return (a > mi) ? mi: mi - 1;
+            }
+            // new middle index
+            mi = a + (b - a + 1) / 2;
         }
-        return 0;   
-    }
-    int mySqrt2(int x) {
-        // What would be not so stupid?
-        // If we computed some sort of mathematical property?
-        // So some Googling reveals that we can do some mathematical manipulation + 
-        // Newton-Raphson method.
-        //
-        // Another method that I came up with is to just use a table. There are a reasonable finite 
-        // amount of perfect square roots like so: 0 <= x <= 231 - 1.
-        // TODO(Noah): implement mySqrt2 to check for speed improvements.
     }
 };
