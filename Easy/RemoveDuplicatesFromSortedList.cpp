6033bb8fb2d8a34071dbaa7c3b6cedcf9926d80e
BluBloos <cnoah1705@gmail.com>
2022-07-26 06:21:57 -0400

    made duplicates from sorted list simpler

--- Easy/RemoveDuplicatesFromSortedList.cpp
+++ Easy/RemoveDuplicatesFromSortedList.cpp
@@ -1,9 +1,3 @@
-// TODO(Noah): Since the list is sorted, there is actually no need to
-// store if things have been seen already. It's more of like this collapsing thing,
-// because we know that duplicates are always adjacent.
-//
-// Also, it is possible to remove the checking for the three different cases?
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -15,31 +9,16 @@
  * };
  */
 class Solution {
-    bool inList[201];
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        memset(inList, 0, sizeof(inList));
+        // Presuming that the list is sorted, this problem becomes incredibly simple.
         ListNode *_head = head;
-        ListNode *prev = head;
+        ListNode *prev = nullptr;
         while (head != nullptr) {
             bool deleted = false;
-            if (deleted = (inList[head->val + 100])) {
-                // remove duplicate
-                // three cases, first node, middle node, end node. 
-                if (head == _head) {
-                    // first node
-                    // this case also handles the first node being simultaneously the first and last.
-                    _head = head->next;
-                } else if (head->next == nullptr) {
-                    // end node
-                    prev->next = nullptr;
-                } else {
-                    // middle node
-                    ListNode *next = head->next;
-                    prev->next = next;
-                }
-            } else {
-                inList[head->val + 100] = true;
+            if (deleted = ((prev != nullptr) ? prev->val == head->val : false)) {
+                ListNode *next = head->next;
+                prev->next = next;
             }
             // advance to next item.
             if (deleted) {
