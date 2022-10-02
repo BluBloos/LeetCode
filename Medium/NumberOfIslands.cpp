527a5589e0cb11f36d76a827b8e3a35515bd53c2
BluBloos <cnoah1705@gmail.com>
2022-10-01 22:14:35 -0400

    are you fu**ing serious? double the speedup on Leetcode by merely not using std::tuple ...

--- Medium/NumberOfIslands.cpp
+++ Medium/NumberOfIslands.cpp
@@ -7,31 +7,33 @@ public:
         // sure: as soon as you hit a 1, do a flood fill outwards.
         // the "visited" arr is embed in the grid, flip the 1s to a 0.
         int numIslands = 0;
-        std::vector<std::tuple<int, int>> stack = {}; 
+        typedef struct pair {
+            int Y;
+            int X;
+        } pair_t;
+        std::vector<pair_t> stack = {}; 
         for (int y = 0; y < m; y++) {
             for (int x = 0; x < n; x++) {
                 if (grid[y][x] == '1') {
                     // flood fill out
                     int Y = y;
                     int X = x;
-                    stack.push_back(std::make_tuple(Y, X));
+                    stack.push_back({Y, X});
                     numIslands++;
                     do {
-                        std::tuple<int, int> myTuple = stack.back();
-                        Y = std::get<0>(myTuple);
-                        X = std::get<1>(myTuple);
+                        pair_t myPair = stack.back();
+                        Y = myPair.Y;
+                        X = myPair.X;
                         stack.pop_back();
-                        if (grid[Y][X] == '1') {
-                            grid[Y][X] = '0';
-                            if (Y + 1 < m)
-                                stack.push_back(std::make_tuple(Y + 1, X));
-                            if (Y - 1 >= 0)
-                                stack.push_back(std::make_tuple(Y - 1, X));
-                            if (X + 1 < n)
-                                stack.push_back(std::make_tuple(Y, X + 1));
-                            if (X - 1 >= 0)
-                                stack.push_back(std::make_tuple(Y, X - 1));
-                        }
+                        grid[Y][X] = '0';
+                        if (Y + 1 < m && grid[Y + 1][X] == '1')
+                            stack.push_back({Y + 1, X});
+                        if (Y - 1 >= 0 && grid[Y - 1][X] == '1')
+                            stack.push_back({Y - 1, X});
+                        if (X + 1 < n && grid[Y][X + 1] == '1')
+                            stack.push_back({Y, X + 1});
+                        if (X - 1 >= 0 && grid[Y][X - 1] == '1')
+                            stack.push_back({Y, X - 1});
                     } while(stack.size() > 0);
                 }
             }
